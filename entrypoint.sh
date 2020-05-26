c794acc2878942484cb85f0c44fe8afb2b1ffd4a
Daniel Augusto Paes Nunes <daniel.nunes@zoop.com.br>
2020-05-26 13:49:58 -0300

    Troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,13 @@ INSTALL_DIR=$3
 cd /docker-action
 echo "creating docker image with Python version: $PYTHON_VERSION"
 
+echo listing /github/workspace
+ls /github/workspace
+
+echo listing curdir
+ls .
+
+echo "GITHUB_WORKSPACE is $GITHUB_WORKSPACE"
+
 docker build -t docker-action --build-arg python_version="$PYTHON_VERSION" --build-arg workspace="$GITHUB_WORKSPACE" --build-arg requirements_file_path="$REQUIREMENTS_FILE_PATH" --build-arg install_dir="$INSTALL_DIR" .
 docker run -v /github/workspace:/github/workspace docker-action
